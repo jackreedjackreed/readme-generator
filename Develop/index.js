3c7472e5da065e1c8c12a70c99c5cda693bcb6e7
J-Boogie <jackr353@gmail.com>
2021-01-15 10:14:37 -0600

    add questions sans licensing

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,44 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require("utils/generateMarkdown.js")
+const generateMarkdown = require('utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input --> github username, email, stuff below
 const questions = [
     {
-        
+        type: "input",
+        name: "github",
+        message: "What's your GitHub username?"
+    },{
+        type: "input",
+        name: "email",
+        message: "What's your email address?"
+    },{
+        type: "input",
+        name: "title",
+        message: "What's the title of this project?"
+    },{
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project."
+    },{
+        type: "input",
+        name: "installation",
+        message: "How do you install this project?"
+    },{
+        type: "input",
+        name: "usage",
+        message: "Describe the purpose of using this project."
+    },{
+        type: "input",
+        name: "contribution",
+        message: "If someone wants to contribute to this project, how should they proceed?"
+    },{
+        type: "input",
+        name: "tests",
+        message: "Describe any tests relevant to this project."
+    },{
+        // need licensing
     }
 ];
 
